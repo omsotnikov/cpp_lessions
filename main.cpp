c986a7611197e9e83cf34c5feedd26edc2af9904
oleg <oleg.sotn@gmail.com>
2020-02-17 15:38:18 +0500

    Splitted declaration and definition of factorial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+// declaration
+int factorial(int n);
+
+// entry point
+int main() {
+
+  std::cout << "Hello world!" << std::endl;
+  std::cout << "The value of 5! is " << factorial(5) << std::endl;
+
+  return 0;
+}
+
+// definition
 int factorial(int n) {
 
   int res = 1;
@@ -9,13 +22,4 @@ int factorial(int n) {
 
   return res;
 
-}
-
-// entry point
-int main() {
-
-  std::cout << "Hello world!" << std::endl;
-  std::cout << "The value of 5! is " << factorial(5) << std::endl;
-
-  return 0;
 }
