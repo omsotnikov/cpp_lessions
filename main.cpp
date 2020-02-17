3d51392c4ba56beb7c1800a139965ffd6017b455
oleg <oleg.sotn@gmail.com>
2020-02-17 15:29:53 +0500

    Added factorial function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+int factorial(int n) {
+
+  int res = 1;
+  for(int i = 1; i <= n; i++) {
+    res = res * i;
+  }
+
+  return res;
+
+}
+
 // entry point
 int main() {
 
   std::cout << "Hello world!" << std::endl;
+  std::cout << "The value of 5! is " << factorial(5) << std::endl;
 
   return 0;
 }
